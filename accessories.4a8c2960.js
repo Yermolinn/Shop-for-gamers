607f5eb35bb87bd6c57aab395a67869fa4c13947
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2022-11-22 07:05:39 +0000

    Deploying to gh-pages from @ Yermolinn/Shop-for-gamers@994198d0c94164217e5322458cbcd2c8e91e8999 🚀

--- accessories.4a8c2960.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=accessories.4a8c2960.js.map
