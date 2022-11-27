a8db0e8094ae227b87adab0d09d663d98e022b68
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2022-11-27 21:50:44 +0200

    Update 3.0

--- src/js/contacts-modal.js
+++ src/js/contacts-modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[contact-modal-open]"),
     closeModalBtn: document.querySelector("[contact-modal-close]"),
     modal: document.querySelector("[contact-modal]"),
+    body: document.querySelector("body"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
