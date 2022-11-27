a9ac900209a14964596d0e7feef8c46af288668b
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2022-11-27 19:51:19 +0000

    Deploying to gh-pages from @ Yermolinn/Shop-for-gamers@a8db0e8094ae227b87adab0d09d663d98e022b68 🚀

---
+++ contact.37a7cb48.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[contact-modal-open]"),closeModalBtn:document.querySelector("[contact-modal-close]"),modal:document.querySelector("[contact-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=contact.37a7cb48.js.map
